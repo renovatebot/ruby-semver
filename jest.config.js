3067aed1bb3b730dde35737a61ee39331295a599
Michael Kriese <michael.kriese@visualon.de>
2023-01-24 10:30:17 +0100

    build: fix package

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,15 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest',
   },
   testEnvironment: 'node',
+  collectCoverage: true,
   coverageProvider: 'v8',
   coverageReporters: ci ? ['html', 'json', 'text'] : ['html', 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
