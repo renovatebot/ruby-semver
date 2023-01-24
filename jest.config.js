7cef293cc2eff1e6ef2fd401e3aa8f7a09204e5e
Michael Kriese <michael.kriese@visualon.de>
2023-01-24 08:45:32 +0100

    fix: relax null checks

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
+
+const ci = !!process.env.CI;
+
+/** @type {import('jest').Config} */
 module.exports = {
   roots: ['<rootDir>/test'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   testEnvironment: 'node',
+  coverageProvider: 'v8',
+  coverageReporters: ci
+  ? ['html', 'json', 'text']
+  : ['html', 'text'],
 };
